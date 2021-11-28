aea3a355b430a0e4b6e705ae8db0dd340c08dfd1
VandersonThiago <vandersontpaulo@gmail.com>
2021-11-28 00:27:06 -0300

    Criado o layout de uma nota aberta | Criado a funcionalidade de remover uma nota | Quando clica em editar uma nota, a nota é aberta com suas informações, permitindo escrever, faltando apenas a funcionalidade de salvar

--- assets/js/notes.js
+++ assets/js/notes.js
@@ -1,9 +1,69 @@
-/*let notes = ['Levar a lola para passear', 'jantar com a mina lá', 'teste atoa de anotação', 'bla bla bla'];
-localStorage.setItem("notes", JSON.stringify(notes))*/
+let selected = null;
+let notes;
+/*let teste = ['teste1', 'teste2', 'teste3', 'teste4', 'teste5'];
+localStorage.setItem("notes", JSON.stringify(teste))*/
 
 /*Get all notes from localStorage*/
+let list = [];
 if(localStorage.getItem("notes")){
-    let list = JSON.parse(localStorage.getItem("notes"));
+    list = JSON.parse(localStorage.getItem("notes"));
+    if(list.length > 0){
+        mountNotes(list);
+    }
+}
+/***/
+
+/*Create a new note*/
+let newNote = document.querySelector('.newNote');
+newNote.addEventListener('click', function(){
+    alert('Criando nova nota')
+});
+/***/
+
+/*Notes events*/
+document.querySelector('.delete').addEventListener('click', function(){
+    if(confirm('Você quer realmente deletar essa nota?'+selected)){
+        list.splice(selected, 1);
+        mountNotes(list)
+        selected = null;
+        localStorage.setItem("notes", JSON.stringify(list))
+        if(list.length < 1){
+            document.querySelector('.boxNotes').innerHTML = '<p class="empty" noteId="">Você não possui nada anotado</p>';
+        }
+    }
+});
+
+document.querySelector('.edit').addEventListener('click', function(){
+    document.querySelector('.openNote').innerText = list[selected];
+    document.querySelector('.BoxOpenNote').style.display = 'flex';
+});
+
+document.querySelector('.cancel').addEventListener('click', function(){
+    document.querySelector('.BoxOpenNote').style.display = 'none';
+});
+/***/
+
+/*Close noteMenu*/
+document.querySelector('.boxNotes').addEventListener('click', function(e){
+    if(this === e.target){
+        document.querySelector('.menuNote').style.display = 'none';
+        newNote.style.bottom = '3%';
+        for(let j = 0; j < notes.length; j++){
+            notes[j].classList.remove('selected');
+        }
+    }
+})
+
+document.querySelector('.closeNoteMenu').addEventListener('click', function(e){
+    if(this === e.target){
+        document.querySelector('.menuNote').style.display = 'none';
+        newNote.style.bottom = '3%';
+    }
+})
+/***/
+
+/*Functions*/
+function mountNotes(list){
 
     document.querySelector('.boxNotes').innerHTML = '';
 
@@ -16,47 +76,25 @@ if(localStorage.getItem("notes")){
         `;
         document.querySelectorAll('.noteSingle')[i].setAttribute('noteId', i)
     }
-
+    sendEventsToNotes()
 }
 
-/*Create a new note*/
-let newNote = document.querySelector('.newNote');
+sendEventsToNotes()
+function sendEventsToNotes(){
+    notes = document.querySelectorAll('.noteSingle');
 
-newNote.addEventListener('click', function(){
-    alert('Criando nova nota')
-});
+    for(let i = 0; i < notes.length; i++){
+        notes[i].addEventListener('click', function(e){
+            let id = notes[i].getAttribute('noteId');
+            selected = id;
+            
+            document.querySelector('.menuNote').style.display = 'flex';
+            newNote.style.bottom = '80px';
 
-
-/*Note single*/
-let notes = document.querySelectorAll('.noteSingle');
-let selected = null;
-
-for(let i = 0; i < notes.length; i++){
-    notes[i].addEventListener('click', function(e){
-        let id = notes[i].getAttribute('noteId');
-        selected = id;
-        
-        document.querySelector('.menuNote').style.display = 'flex';
-        newNote.style.bottom = '80px';
-
-        for(let j = 0; j < notes.length; j++){
-            notes[j].classList.remove('selected');
-        }
-        notes[i].classList.add('selected');
-    })
+            for(let j = 0; j < notes.length; j++){
+                notes[j].classList.remove('selected');
+            }
+            notes[i].classList.add('selected');
+        })
+    }
 }
-
-/*Close noteMenu*/
-document.querySelector('.boxNotes').addEventListener('click', function(e){
-    if(this === e.target){
-        document.querySelector('.menuNote').style.display = 'none';
-        newNote.style.bottom = '3%';
-    }
-})
-
-document.querySelector('.closeNoteMenu').addEventListener('click', function(e){
-    if(this === e.target){
-        document.querySelector('.menuNote').style.display = 'none';
-        newNote.style.bottom = '3%';
-    }
-})
